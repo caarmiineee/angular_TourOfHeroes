40d50911a9e13c5a6adb69ac53512dc82ffa45d3
carmine oliveti <carmineolivetti@gmail.com>
2022-08-04 15:56:31 +0200

    first commit

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,43 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Hero } from '../hero.entity';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  hero: Hero = {
+    id: 50,
+    name: 'cristopher'
+  };
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    window.location.href = "/";
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+  }
+}
